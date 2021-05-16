0013096a0dd454d92f611a225a93b19774efab4d
Mark Boger <mboger@indigital.net>
2021-05-16 14:12:53 -0400

    Update test app

--- test.lisp
+++ test.lisp
@@ -9,15 +9,27 @@
         <div>Hello, World</div>
         <br/>
         <div id=\"click\">Counter:<span id=\"count\">0</span></div>
+        <button id=\"inc\">Increment</button>
+        <button id=\"dec\">Decrement</button>
     </body>
     <script>
         window.onload = function() {
             let count = document.getElementById('count');
-            let click = document.getElementById('click');
+            let inc = document.getElementById('inc');
+            let dec = document.getElementById('dec');
 
-            click.onclick = async () => {
+            inc.onclick = async () => {
                 try {
-                    let new_count = await counter();
+                    let new_count = await inc_counter();
+                    count.innerHTML = new_count;
+                } catch (err) {
+                    alert(err);
+                }
+            }
+
+            dec.onclick = async () => {
+                try {
+                    let new_count = await dec_counter();
                     count.innerHTML = new_count;
                 } catch (err) {
                     alert(err);
@@ -36,8 +48,11 @@
 
 (defparameter *counter* 0)
 
-(defbinding *w* counter ()
+(defbinding *w* inc_counter ()
   (incf *counter*))
+
+(defbinding *w* dec_counter ()
+  (decf *counter*))
 
 (run *w*)
 
